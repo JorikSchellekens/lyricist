ad01252254b2918c779b1e3790a78cef3aff80fa
Jorik Schellekens <JorikSch@gmail.com>
2018-04-10 02:13:50 +0100

    Fix import

--- js/utils/fetchLyrics.js
+++ js/utils/fetchLyrics.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-
+import { sandAZParams } from './sandSongTitle';
 import { AZLYRICS_TEMPLATE } from '../constants'
+
 export const fetchAZLyrics = (metadata) => {
   const options = {
     method: 'GET',
-    url: AZLYRICS_TEMPLATE(sandAZParams(metatdata)),
+    url: AZLYRICS_TEMPLATE(sandAZParams(metadata)),
   };
   return axios(options);
 }
