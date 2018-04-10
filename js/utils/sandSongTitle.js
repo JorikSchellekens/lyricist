44b180ce55084976476a879b17ded87925d572f5
Jorik Schellekens <JorikSch@gmail.com>
2018-04-10 02:22:26 +0100

    Fix order in metadata object

--- js/utils/sandSongTitle.js
+++ js/utils/sandSongTitle.js
@@ -3,14 +3,13 @@ import { YOUTUBE_TAG_REGEX } from '../constants';
 export const sandYoutubeTag = tag => {
   const matches = tag.match(YOUTUBE_TAG_REGEX);
   return {
-    artist: matches[1],
-    title: matches[2],
+    artist: matches[2],
+    title: matches[1],
   }
 }
 
 export const sandAZParams = (metadata) => {
   const { artist, title } = metadata;
-  console.log(artist, title);
   const applySand = s => s.toLowerCase().split('').filter(c => 'a' <= c && c <= 'z').join('');
   return {
     artist: applySand(artist),
