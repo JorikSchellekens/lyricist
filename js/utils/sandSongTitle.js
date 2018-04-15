a03a7dc96f3299d8d7a7f7e4c4814217658c0070
Jorik Schellekens <JorikSch@gmail.com>
2018-04-15 23:57:06 +0100

    Fix group selector bug.
    Artist now in field 3.

--- js/utils/sandSongTitle.js
+++ js/utils/sandSongTitle.js
@@ -3,7 +3,7 @@ import { YOUTUBE_TAG_REGEX } from '../constants';
 export const sandYoutubeTag = tag => {
   const matches = tag.match(YOUTUBE_TAG_REGEX);
   return {
-    artist: matches[2],
+    artist: matches[3],
     title: matches[1],
   }
 }
