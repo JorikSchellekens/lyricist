6755283ab21aed9793ad688eab9322aff3798ffc
Swapnil Raj <raj.swapnil99@gmail.com>
2018-04-15 22:59:37 +0100

    Update YOUTUBE_TAG_REGEX to support different variants

--- js/constants.js
+++ js/constants.js
@@ -3,7 +3,7 @@ export const LYRIC_LOCATION_SELECTOR = '#items';
 export const MUSIC_TAG_SELECTOR = '#collapsible ytd-metadata-row-renderer:last-child #content';
 export const SHOW_MORE_SELECTOR = 'paper-button.ytd-expander:nth-child(4)';
 
-export const YOUTUBE_TAG_REGEX = /"(.*)" by (.*) \( • \)/;
+export const YOUTUBE_TAG_REGEX = /"([^()]*)(.*)?" by (.*) \(.*•?.*\)/;
 export const AZLYRICS_TEMPLATE = (metadata) => `https://www.azlyrics.com/lyrics/${metadata.artist}/${metadata.title}.html`;
 export const HTML_MIME_TYPE = 'text/html';
 
