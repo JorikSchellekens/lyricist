f76c21de751d539030b70a73636fbaa949e54b1b
Jorik Schellekens <JorikSch@gmail.com>
2018-04-16 02:47:40 +0100

    Undo simplified selectors to unbreak Firefox

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 export const LYRIC_LOCATION_SELECTOR = '#items';
 export const MUSIC_TAG_SELECTOR = '#collapsible ytd-metadata-row-renderer:last-child #content';
-export const SHOW_MORE_SELECTOR = '#more';
-export const SHOW_LESS_SELECTOR = '#less';
+export const SHOW_MORE_SELECTOR = 'paper-button.ytd-expander:nth-child(4)';
+export const SHOW_LESS_SELECTOR = 'paper-button.ytd-expander:nth-child(3)';
 
 export const YOUTUBE_TAG_REGEX = /"([^()]*)(.*)?" by (.*) \(.*•?.*\)/;
 
