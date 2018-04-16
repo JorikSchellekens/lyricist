f39a60c8da957f9d6ae175fe9d91ce8777022f12
Swapnil Raj <raj.swapnil99@gmail.com>
2018-04-16 03:55:18 +0100

    Add styling on lyrics view
    * Add styling object to constants

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,8 @@
 export const LYRIC_LOCATION_SELECTOR = '#items';
+export const LYRICS_VIEW_CSS = {
+  'color': 'var(--ytd-video-primary-info-renderer-title-color, var(--yt-primary-text-color))',
+  'font-family': 'var(--ytd-video-primary-info-renderer-title-font-family, inherit)',
+  'font-size': '1.4rem',
+  'overflow': 'auto',
+  'height': '360px',
+};
--- js/views/lyricsView.js
+++ js/views/lyricsView.js
@@ -2,8 +2,15 @@ import { createDiv, injectDOMElement } from '../utils/DOMUtils';
 import {
   COMMENT_REGEX,
   LYRIC_LOCATION_SELECTOR,
+  LYRICS_VIEW_CSS,
 } from '../constants';
 
 export const createLyricView = lyrics => {
-  injectDOMElement(LYRIC_LOCATION_SELECTOR, createDiv(lyrics));
+  const lyricsDiv = createDiv(lyrics);
+  addCSS(LYRICS_VIEW_CSS, lyricsDiv);
+  injectDOMElement(LYRIC_LOCATION_SELECTOR, lyricsDiv);
+}
+
+const addCSS = (styles, element) => {
+  Object.entries(styles).forEach(pair => element.style[pair[0]] = [pair[1]]);
 }
