f294d027ec5b4e93f23e3faeb59da3913f49236d
Owen Shepherd <414owen@gmail.com>
2018-12-24 18:57:05 +0000

    style-points

--- js/utils/DOMUtils.js
+++ js/utils/DOMUtils.js
@@ -4,18 +4,18 @@ export const createDiv = (innerText = '') => {
   const element = document.createElement('div');
   element.innerHTML = innerText;
   return element;
-}
+};
 
 export const injectDOMElement = (selector, element) => {
   const root = document.querySelector(selector);
   root.insertBefore(element, root.firstChild);
-}
+};
 
-export const removeDOMElement = (element) => {
-  if(element) element.parentNode.removeChild(element);
-}
+export const removeDOMElement = element => {
+  if (element) element.parentNode.removeChild(element);
+};
 
 export const parseDOM = result => {
   const DOM = new DOMParser();
   return DOM.parseFromString(result.data, HTML_MIME_TYPE);
-}
+};
--- js/utils/awaitElement.js
+++ js/utils/awaitElement.js
@@ -1,21 +1,18 @@
-const awaitElement = (selector, timeout = 30000) => {
-  return new Promise((resolve, reject) => {
+const awaitElement = (selector, timeout = 30000) =>
+  new Promise((resolve, reject) => {
     const limit = Date.now() + timeout;
     const intervalId = setInterval(() => {
       /**
        * @type {HTMLElement} el
        */
       const el = document.querySelector(selector);
-       if (el) {
-         clearInterval(intervalId);
-         resolve(el);
-       } else if (Date.now() >= limit) {
-         reject(new Error(`Timeout: ${selector} not found`));
-       }
-
-      }, 500);
-
+      if (el) {
+        clearInterval(intervalId);
+        resolve(el);
+      } else if (Date.now() >= limit) {
+        reject(`Timeout: ${selector} not found`);
+      }
+    }, 500);
   });
-}
 
 export default awaitElement;
