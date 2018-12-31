43cc79b387d7d844b36015cdcb1874c27d46f857
Swapnil Raj <raj.swapnil99@gmail.com>
2019-01-01 02:12:14 +0530

    Add trailing commas to the data for testing
    Add comments about the suffix

--- test/unit/utils/parsers/title.test.js
+++ test/unit/utils/parsers/title.test.js
@@ -13,17 +13,19 @@ test('Song titles with the - separator', () => {
   [
     [
       'Kasabian - Re-wired',
-      metaDataObject('Kasabian', 'Re-wired')
+      metaDataObject('Kasabian', 'Re-wired'),
     ],
     [
       'Dylan, Bob - A Hard Rain\'s A-Gonna Fall',
-      metaDataObject('Dylan, Bob', 'A Hard Rain\'s A-Gonna Fall')
+      metaDataObject('Dylan, Bob', 'A Hard Rain\'s A-Gonna Fall'),
     ],
     [
       'Lekman, Jens - Friday Night at the Drive-in Bingo',
-      metaDataObject('Lekman, Jens', 'Friday Night at the Drive-in Bingo')
+      metaDataObject('Lekman, Jens', 'Friday Night at the Drive-in Bingo'),
     ],
   ]
+  // Add the ` - YouTube` suffix which is added by YouTube to the document
+  // title.
   .map(([ fullname, songObject ]) => [ `${fullname} - YouTube`, songObject ])
   .forEach(([ fullname, songObject ]) => {
     document.title = fullname;
@@ -39,29 +41,31 @@ test('Song titles with separators other than -', () => {
 [
     [
       'Kasabian:Rewired',
-      metaDataObject('Kasabian', 'Rewired')
+      metaDataObject('Kasabian', 'Rewired'),
     ],
     [
       'Kasabian : Rewired',
-      metaDataObject('Kasabian', 'Rewired')
+      metaDataObject('Kasabian', 'Rewired'),
     ],
     [
       'Dylan, Bob///A Hard Rain\'s A-Gonna Fall',
-      metaDataObject('Dylan, Bob', 'A Hard Rain\'s A-Gonna Fall')
+      metaDataObject('Dylan, Bob', 'A Hard Rain\'s A-Gonna Fall'),
     ],
     [
       'Dylan, Bob /// A Hard Rain\'s A-Gonna Fall',
-      metaDataObject('Dylan, Bob', 'A Hard Rain\'s A-Gonna Fall')
+      metaDataObject('Dylan, Bob', 'A Hard Rain\'s A-Gonna Fall'),
     ],
     [
       'Lekman, Jens|Friday Night at the Drive-in Bingo',
-      metaDataObject('Lekman, Jens', 'Friday Night at the Drive-in Bingo')
+      metaDataObject('Lekman, Jens', 'Friday Night at the Drive-in Bingo'),
     ],
     [
       'Lekman, Jens | Friday Night at the Drive-in Bingo',
-      metaDataObject('Lekman, Jens', 'Friday Night at the Drive-in Bingo')
+      metaDataObject('Lekman, Jens', 'Friday Night at the Drive-in Bingo'),
     ],
   ]
+  // Add the ` - YouTube` suffix which is added by YouTube to the document
+  // title.
   .map(([ fullname, songObject ]) => [ `${fullname} - YouTube`, songObject ])
   .forEach(([ fullname, songObject ]) => {
     document.title = fullname;
